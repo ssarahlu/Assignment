08327e0eb1ac9b4aad727dbfe037ce04fc942dd1
vanessalay <vanessaalay@gmail.com>
2020-04-26 16:25:19 +1000

    Music Trivia quiz completed

--- app/src/main/java/com/example/assignment/QuizActivity.java
+++ app/src/main/java/com/example/assignment/QuizActivity.java
@@ -6,11 +6,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class QuizActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigation;
+    Button start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +22,19 @@ public class QuizActivity extends AppCompatActivity {
         setContentView(R.layout.activity_quiz);
         setTitle("Quiz");
         bottomNavigation = findViewById(R.id.navigation);
+        start = findViewById(R.id.start);
+
         bottomNavigation.setItemIconTintList(null);
         bottomNavigation.setItemTextColor(null);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SelectQuizDifficulty.class);
+                startActivity(intent);
+            }
+        });
+
         bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
